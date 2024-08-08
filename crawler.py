cac4b3d7880e52e060d059aa7bb86577187c6b13
pilyeooong <pilyeooong@gmail.com>
2024-08-08 21:04:47 +0900

    add. crawler

--- crawler.py
+++ crawler.py
@@ -1,15 +1,31 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-url = 'https://example.com'
+def is_flight_schedule_available():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
 
-response = requests.get(url)
+    url = 'https://sky.interpark.com/schedules/domestic/CJU-GMP-20240918?adt=2&chd=0&inf=0&seat=ALL&pickAirLine=&pickMainFltNo=&pickSDate='
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    for link in soup.find_all('a'):
-        print('텍스트:', link.text)
-        print('링크:', link.get('href'))
-else:
-    print('웹 페이지를 불러오는 데 실패했습니다.', response.status_code)
+    driver.get(url)
+
+    try:
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div.scheduleList > div.tblbody > div.scrollArea'))
+        )
+        
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        
+        scroll_area_div = soup.select_one('div.scheduleList > div.tblbody > div.scrollArea')
+        if scroll_area_div:
+            return True
+        
+    except:
+        return False
+
+    finally:
+        driver.quit()
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def hi():
-    print('hi')
+import os
+from crawler import is_flight_schedule_available
+from slack import send_slack_webhook
 
-hi()
+def main():
+    print('start to check flight schedule')
+    is_available = is_flight_schedule_available()
+    if is_available:
+        webhook_url = os.getenv('SLACK_WEBHOOK_URL')
+        message = 'flight schedule available !'
+        send_slack_webhook(webhook_url, message)
+
+
+if __name__ == "__main__":
+    main()
