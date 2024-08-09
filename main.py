d325cb0ce3ec6bd455d6648e82d6082d1b66a8f1
pilyeooong <pilyeooong@gmail.com>
2024-08-09 11:59:56 +0900

    add. interval

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from slack import send_slack_webhook
 load_dotenv()
 
 def main():
-    while True:
-        print('start to check flight schedule')
-        is_available = is_flight_schedule_available()
-        if is_available:
-            webhook_url = os.getenv('SLACK_WEBHOOK_URL')
-            message = 'flight schedule available !'
+    webhook_url = os.getenv('SLACK_WEBHOOK_URL')
+    message = 'flight schedule available !'
 
+    while True:
+        print('Start to check flight schedule')
+
+        is_available = is_flight_schedule_available()
+
+        if is_available:
             # TODO: 비행기 스케줄 시간도 긁어서 보내기
             send_slack_webhook(webhook_url, message)
         time.sleep(30)
