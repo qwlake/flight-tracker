0194b4e42957e1579a958b9d7a6417d3fffed8a2
pilyeooong <pilyeooong@gmail.com>
2024-08-09 11:50:49 +0900

    add. webdriver arguments

--- crawler.py
+++ crawler.py
@@ -9,12 +9,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def is_flight_schedule_available():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
     flight_schedule_date = os.getenv('FLIGHT_SCHEDULE_DATE')
     url = f'https://sky.interpark.com/schedules/domestic/CJU-GMP-{flight_schedule_date}?adt=2&chd=0&inf=0&seat=DOMESTIC_BASE&pickAirLine=&pickMainFltNo=&pickSDate='
 
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--remote-debugging-port=9222") 
+
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     try:
