aeb055d72cd3340476aa7de2f8cc5bc829eff495
pilyeooong <pilyeooong@gmail.com>
2024-08-09 11:44:36 +0900

    add. procfile

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from dotenv import load_dotenv
 import os
 from crawler import is_flight_schedule_available
@@ -6,14 +7,16 @@ from slack import send_slack_webhook
 load_dotenv()
 
 def main():
-    print('start to check flight schedule')
-    is_available = is_flight_schedule_available()
-    if is_available:
-        webhook_url = os.getenv('SLACK_WEBHOOK_URL')
-        message = 'flight schedule available !'
+    while True:
+        print('start to check flight schedule')
+        is_available = is_flight_schedule_available()
+        if is_available:
+            webhook_url = os.getenv('SLACK_WEBHOOK_URL')
+            message = 'flight schedule available !'
 
-        # TODO: 비행기 스케줄 시간도 긁어서 보내기
-        send_slack_webhook(webhook_url, message)
+            # TODO: 비행기 스케줄 시간도 긁어서 보내기
+            send_slack_webhook(webhook_url, message)
+        time.sleep(30)
 
 
 if __name__ == "__main__":
