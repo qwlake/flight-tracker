5f962af4036ba88644140cb098fab04ceba52f0b
Oscar <jungwoo.oh@carrotins.com>
2025-03-14 15:48:55 +0900

    출력 메세지에 날짜 추가

--- main.py
+++ main.py
@@ -13,23 +13,31 @@ def main():
         urls = os.getenv('FLIGHT_SCHEDULE_URL')
         webhook_url = os.getenv('SLACK_WEBHOOK_URL')
 
-        schedules = []
+        schedules_map = dict()
         try:
             for url in urls.split('\\'):
-                schedules += get_flight_schedules(url)
+                schedules = get_flight_schedules(url)
+                if schedules:
+                    schedules_map[url[-8:]] = schedules
 
-            if schedules:
-                message = "<!channel>\n" + "\n".join([
-                    f"*Airline:* {schedule['airline_name']}\n*Departure:* {schedule['departure_time']} - *Arrival:* {schedule['arrival_time']}\n*Fee:* {schedule['fee']}\n"
+            message = ''
+
+            for date, schedules in schedules_map.items():
+                if message == '':
+                    message = "<!channel>\n"
+                message += "\n".join([
+                    f"*Airline:* {schedule['airline_name']}\n*Departure:* {date} {schedule['departure_time']} - *Arrival:* {date} {schedule['arrival_time']}\n*Fee:* {schedule['fee']}\n"
                     for schedule in schedules
                 ])
-            else:
+
+            if message == '':
                 message = "예약 가능 스케줄이 없습니다."
 
         except Exception as e:
             message = f"Error sending message: {e}\n{traceback.format_exc()}"
 
         send_slack_webhook(webhook_url, message)
+        print(message)
 
         random_number = random.randint(30, 60)
         time.sleep(random_number)
